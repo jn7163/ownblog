28a59403b0c8e3a8e81e9a7941a93569164f4c58
lcy <lichunyang@stu.xidian.edu.cn>
2017-01-11 21:52:55 +0800

    change URL from IP to lcyown.cn

--- oauth/urls.py
+++ oauth/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^github$', views.oauth_github,name='oauth_github'),
-    url(r'^tencent$', views.oauth_tencent,name='oauth_tencent'),
-    url(r'^tencent/get$', views.oauth_tencent_get,name='oauth_tencent_get'),
+    url(r'^github', views.oauth_github,name='oauth_github'),
+    url(r'^tencent', views.oauth_tencent,name='oauth_tencent'),
+    url(r'^tencent/get', views.oauth_tencent_get,name='oauth_tencent_get'),
 ]
